3f9b91d107289002b06282b4b40c8258946a2a01
phelix <github@blockchained.com>
2015-07-11 15:17:06 +0200

    gather_files coin specific release folder to prevent overwriting builds of other coins

--- 5_gather_files.bat
+++ 5_gather_files.bat
@@ -1,13 +1,13 @@
 @call set_vars.bat
 
 @rem clean up previous files if any
-@if not exist %RELEASE% mkdir %RELEASE%
-@del %RELEASE%\*.* /S /Q /F
+@if not exist %COINFOLDER%-%RELEASE% mkdir %COINFOLDER%-%RELEASE%
+@del %COINFOLDER%-%RELEASE%\*.* /S /Q /F
 
 @echo Gather executables
-copy %COINFOLDER%\src\*.exe %RELEASE%\
-copy %COINFOLDER%\src\qt\*.exe %RELEASE%\
+copy %COINFOLDER%\src\*.exe %COINFOLDER%-%RELEASE%\
+copy %COINFOLDER%\src\qt\*.exe %COINFOLDER%-%RELEASE%\
 
-@dir %RELEASE%
+@dir %COINFOLDER%-%RELEASE%
 
 @if not "%RUNALL%"=="1" pause
