01885e023dd676c629d98692e755bbc8ef6d7a8c
phelix <github@blockchained.com>
2015-07-11 14:57:31 +0200

    1_environment: fix bin path

--- 1_environment.bat
+++ 1_environment.bat
@@ -8,16 +8,16 @@
 @echo.
 @echo Once the mingw-get-setup has finished press a key.
 @pause
-%MINGWPATH%\mingw-get.exe update
-%MINGWPATH%\mingw-get.exe install msys-base
-%MINGWPATH%\mingw-get install msys-wget-bin
-%MINGWPATH%\mingw-get install msys-unzip-bin
-%MINGWPATH%\mingw-get install msys-perl
+%MINGWPATH%\bin\mingw-get.exe update
+%MINGWPATH%\bin\mingw-get.exe install msys-base
+%MINGWPATH%\bin\mingw-get install msys-wget-bin
+%MINGWPATH%\bin\mingw-get install msys-unzip-bin
+%MINGWPATH%\bin\mingw-get install msys-perl
 
-%MINGWPATH%\mingw-get install msys-base-bin
-%MINGWPATH%\mingw-get install msys-autoconf-bin
-%MINGWPATH%\mingw-get install msys-automake-bin
-%MINGWPATH%\mingw-get install msys-libtool-bin
+%MINGWPATH%\bin\mingw-get install msys-base-bin
+%MINGWPATH%\bin\mingw-get install msys-autoconf-bin
+%MINGWPATH%\bin\mingw-get install msys-automake-bin
+%MINGWPATH%\bin\mingw-get install msys-libtool-bin
 
 echo.
 echo done
